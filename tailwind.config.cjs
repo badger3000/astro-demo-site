a421a42c4300f79e8ac251bfbe56159e2e3635ee
Kyle Ross <kyle@badger3000.com>
2023-02-08 23:21:03 -0700

    basic setup, added fonts , images and updated readme. started layout and navigation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "sans": ["Plus Jakarta Sans"],
+      },
+      container: {
+        center: true,
+      },
+      screens: {
+        "tablet": "640px",
+        // => @media (min-width: 640px) { ... }
+
+        "laptop": "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        "desktop": "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
+      backgroundImage: {
+        "hero-image": "url('./public/images/hero-image.webp')",
+      },
+    },
+  },
+  plugins: [],
+};
